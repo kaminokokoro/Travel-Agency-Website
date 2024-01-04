387a5f44c6aca7cab3cce36ee71dda305205fae2
itsZiang <letruonggiang7gpl@gmail.com>
2024-01-04 21:55:57 +0700

    Revert "j"
    This reverts commit 5162cb7d3ad2b624d348d63c3870b74597b7d59e.

--- backend/db/db.py
+++ backend/db/db.py
@@ -8,8 +8,7 @@ from backend.db.model import Base, User
 
 
 # from model import User,Base
-connection_string = "mysql+mysqlconnector://root:alotelope@localhost/travel_agency"
-# connection_string = "mysql+pymysql:root:@localhost:3306/travelAgency"
+connection_string = "mysql+mysqlconnector://root:mocbop123@localhost/travel_agency_v1"
 engine = create_engine(connection_string, echo=True)
 
 
